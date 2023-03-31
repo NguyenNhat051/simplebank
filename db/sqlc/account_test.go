325dfb537a711527fd2425ca372e93a4d6879f5c
Neil <nguyennhat66993@gmail.com>
2023-03-31 17:48:42 +0700

    Fix test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -5,14 +5,15 @@ import (
 	"database/sql"
 	"simplebank/util"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func createRandomAccount(t *testing.T) Account {
+	user := createRandomUser(t)
+
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
@@ -32,61 +33,7 @@ func createRandomAccount(t *testing.T) Account {
 }
 
 func TestCreateAccount(t *testing.T) {
-	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
-		Balance:  util.RandomMoney(),
-		Currency: util.RandomCurrency(),
-	}
-
-	account, err := testQueries.CreateAccount(context.Background(), arg)
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
-
-	require.Equal(t, arg.Owner, account.Owner)
-	require.Equal(t, arg.Balance, account.Balance)
-	require.Equal(t, arg.Currency, account.Currency)
-
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
-}
-
-func TestGetAccount(t *testing.T) {
-	tempAccount := createRandomAccount(t)
-
-	account, err := testQueries.GetAccount(context.Background(), tempAccount.ID)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
-
-	require.Equal(t, tempAccount.Owner, account.Owner)
-	require.Equal(t, tempAccount.Balance, account.Balance)
-	require.Equal(t, tempAccount.Currency, account.Currency)
-	require.WithinDuration(t, tempAccount.CreatedAt, account.CreatedAt, time.Second)
-
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
-}
-
-func TestUpdateAccount(t *testing.T) {
-	tempAccount := createRandomAccount(t)
-
-	args := UpdateAccountParams{
-		ID:      tempAccount.ID,
-		Balance: util.RandomMoney(),
-	}
-
-	account, err := testQueries.UpdateAccount(context.Background(), args)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
-
-	require.Equal(t, tempAccount.Owner, account.Owner)
-	require.Equal(t, account.Balance, args.Balance)
-	require.Equal(t, tempAccount.Currency, account.Currency)
-	require.WithinDuration(t, tempAccount.CreatedAt, account.CreatedAt, time.Second)
-
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
+	createRandomAccount(t)
 }
 
 func TestDeleteAccount(t *testing.T) {
